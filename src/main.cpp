022c5f6d849fa0cd346bbafab9a9b4c0561dce0d
Alexander Korzun <korzun.sas@mail.ru>
2018-11-09 14:08:37 +0300

    Add some demo & fix sh3 build system

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ int main() {
 
     auto get_delta = [](double angle) {
         int offset_x = cos(angle) * 30;
-        int offset_y = sin(angle) * 15;
+        int offset_y = sin(angle) * 15 / 2;
         return ccom::AbsolutePoint(60 + offset_x, 20 + offset_y);
     };
 
@@ -29,18 +29,24 @@ int main() {
     };
     
     auto& draw = ccom::get_draw_manager();
-    draw.set_buffer_size(width, height, '.');
+    draw.set_buffer_size(width, height, ':');
     auto& draw_loop = ccom::get_draw_loop_manager();
-    draw_loop.set_max_fps(100);
+    draw_loop.set_max_fps(30);
     
     double angle = 0;
     draw_loop.run_draw_loop([&](double fps, double elapsed_time, double frame_time) {
-        draw.fill_triangle(get_triangle(angle), '#');
+        auto tri = get_triangle(angle);
+        draw.fill_triangle(tri, '=');
+        draw.draw_line(ccom::AbsoluteLine({15, 19}, tri.a), ' ');
+        draw.draw_line(ccom::AbsoluteLine({15, 19}, tri.b), ' ');
+        draw.draw_line(ccom::AbsoluteLine({15, 19}, tri.c), ' ');
+        draw.draw_line(ccom::AbsoluteLine({30, 10}, tri.a), '@');
+        draw.draw_line(ccom::AbsoluteLine({30, 10}, tri.b), '@');
+        draw.draw_line(ccom::AbsoluteLine({30, 10}, tri.c), '@');
+
         draw.flush_buffer(std::cout);
-        draw.clear_buffer('.');
-        std::cout << std::setprecision(2) << fps << " fps, " << elapsed_time << ", " << frame_time << std::endl;
+        draw.clear_buffer(':');
+        std::cout << "\x1b[A\x1b[2K" << "FPS: " << fps << ",\tElapsed time: " << elapsed_time << ",\tFrame time:" << frame_time << std::endl;
         angle += frame_time;
     });
-    while (true) {
-    }
 }
